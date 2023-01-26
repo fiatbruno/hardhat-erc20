d2a87097df8317df2391a9834ec2b1268baed86d
Fiat Bruno <fiatbruno10@gmail.com>
2023-01-26 10:14:19 +0200

    Added transferFrom() and approve() functions"

--- contract/ManualToken.sol
+++ contract/ManualToken.sol
@@ -11,6 +11,10 @@ contract ManualToken {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
+    event Transfer(address indexed from, address indexed to, uint256 indexed value);
+
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
     constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol) {
         totalSupply = initialSupply * 10 ** uint256(decimals);
         balanceOf[msg.sender] = totalSupply;
@@ -28,11 +32,32 @@ contract ManualToken {
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
 
+        emit Transfer(_from, _to, _value);
+
         assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
         return true;
     }
 }
