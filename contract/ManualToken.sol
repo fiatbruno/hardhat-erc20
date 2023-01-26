1a17114763c7bd8ba3a4d53e652c2386fc382864
Fiat Bruno <fiatbruno10@gmail.com>
2023-01-26 10:37:05 +0200

    Finished ERC20 token implementation

--- contract/ManualToken.sol
+++ contract/ManualToken.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+interface tokenRecepiet {
+    function recieveApproval(
+        address _from,
+        uint256 _value,
+        address _token,
+        bytes calldata _extraData
+    ) external;
+}
+
 contract ManualToken {
     string public name;
     string public symbol;
@@ -14,6 +23,8 @@ contract ManualToken {
     event Transfer(address indexed from, address indexed to, uint256 indexed value);
 
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    event Burn(address indexed from, uint256 value);
 
     constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol) {
         totalSupply = initialSupply * 10 ** uint256(decimals);
@@ -58,6 +69,40 @@ contract ManualToken {
 
         emit Approval(msg.sender, _spender, _value);
 
+        return true;
+    }
+
+    function approveAndCall(
+        address _spender,
+        uint256 _value,
+        bytes calldata _extraData
+    ) public returns (bool success) {
+        tokenRecepiet spender = tokenRecepiet(_spender);
+        if (approve(_spender, _value)) {
+            spender.recieveApproval(msg.sender, _value, address(this), _extraData);
+            return true;
+        }
+    }
+
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+
+        emit Burn(msg.sender, _value);
+
+        return true;
+    }
+
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        allowance[_from][msg.sender] -= _value;
+        totalSupply -= _value;
+
+        emit Burn(_from, _value);
         return true;
     }
 }
