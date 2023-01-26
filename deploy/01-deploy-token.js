d60a5d08b8b592e971212b6a2bf8f7fadca4608a
Fiat Bruno <fiatbruno10@gmail.com>
2023-01-26 08:54:56 +0200

    initial commit

---
+++ deploy/01-deploy-token.js
@@ -0,0 +1,8 @@
+const { network } = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+};
+
+module.exports.tags = ["all", "token"];
